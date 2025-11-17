5dfc9b98edd36c0bcbc0c7f6dec6d150e685939b
Inyangaa <xobofactors@aol.com>
2025-11-17 15:55:25 -0500

    Add Footer component with copyright notice

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <p className="text-base text-gray-400">
+            © 2024 Zoer App. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
