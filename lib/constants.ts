bf07dc066fd58da9fd34e15ef5bda603733a7f18
Inyangaa <xobofactors@aol.com>
2025-11-17 16:03:31 -0500

    Add application constants and routes

---
+++ lib/constants.ts
@@ -0,0 +1,14 @@
+export const APP_NAME = 'Zoer'
+export const APP_DESCRIPTION = 'A modern Next.js application'
+export const APP_VERSION = '1.0.0'
+
+export const ROUTES = {
+  HOME: '/',
+  ABOUT: '/about',
+  CONTACT: '/contact',
+  DOCS: '/docs',
+} as const
+
+export const API_ENDPOINTS = {
+  HELLO: '/api/hello',
+} as const
